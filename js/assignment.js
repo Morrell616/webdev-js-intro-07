909d9ee3b17b4563c39d6e194ac48b46cfbda4a2
Jen Morrell <morrell616@hotmail.com>
2025-01-07 20:25:05 -0500

    I finally figured it out!

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,26 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+
+function returnTheRemainder(num1, num2) {
+      return num1 % num2;
+}
+
+function checkIfEven(number) {
+    return number % 2 === 0;
+}
+
+function getTheFourthElement() {
+    return lostNumbers[3];
+}
+function render () {
+   remainderElement.textContent = returnTheRemainder(5, 4);
+   checkIfEvenElement.textContent = checkIfEven(47);
+   lostNumbersElement.textContent = getTheFourthElement();
+}
+submissionBtn.addEventListener("click", function () {
+    // Call the render function
+    render();
+});
+
--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+
+function greet(name = 'guest') {
+    console.log('Hello, ' + name + '!');
+    greet ("Jen");
+}
+function add(num1, num2) {
+    return num2 + num1;
+}
+
+let sum= add(6, 2);
+console.log(sum);
+*/
 
